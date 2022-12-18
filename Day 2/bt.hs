ed94ba3eb761bada595ff5a15aa497672f47a09f
Nguyễn Minh <sieuquaytheky2001@gmail.com>
2022-12-18 16:32:44 +0000

    Update Day 2

--- Day 2/bt.hs
+++ Day 2/bt.hs
@@ -2,25 +2,32 @@
 -- Add the type signatures for the functions below and then remove the comments and try to compile.
 -- (Use the types presented in the lecture.)
 
--- f1 x y z = x ** (y/z)
-f1 x y z = x ** (y/z)
-3125
+f1 :: Double -> Double -> Double -> Double
+f1 x y z = x ** (y / z)
+
 -- ** là số mũ
 
--- f2 x y z = sqrt (x/y - z)
-f2 50 2 0
+f2 :: Float -> Float -> Float -> Float
+f2 x y z = sqrt (x / y - z)
 
--- f3 x y = [x == True] ++ [y]
-f3 True False
+--f2 50 2 0
 
--- f4 x y z = x == (y ++ z)
-f4 "True" "Tr" "ue"
-f4 "True" ['T','r'] ['u','e']
+f3 :: Bool -> Bool -> [Bool]
+f3 x y = [x == True] ++ [y]
+
+--f3 True False
+
+f4 :: [Char] -> [Char] -> [Char] -> Bool
+--f4:: [Int]->[Int]->[Int]->Bool
+--f4:: [Bool]->[Bool]->[Bool]->Bool
+f4 x y z = x == (y ++ z)
+
+--f4 "True" "Tr" "ue"
+--f4 "True" ['T','r'] ['u','e']
 
 -- Question 2
 -- Why should we define type signatures of functions? How can they help you? How can they help others?
 Để khai báo định dạng đầu ra của hàm
-
 
 -- Question 3
 -- Why should you define type signatures for variables? How can they help you?
@@ -29,18 +36,19 @@ f4 "True" ['T','r'] ['u','e']
 -- Question 4
 -- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
 
-GHCi> show 6
+GHCi > show 6
 "6"
-GHCi> show 'c'
+GHCi > show 'c'
 "'c'"
-GHCi>show 6.0
+GHCi > show 6.0
 "6.0"
 
 printDouble :: Int -> String
-printDouble value = show (value*2)
+printDouble value = show (value * 2)
 
 -- Question 5
 -- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner most elements?
 
-l = [[1,2,3],[4,5,6],[7,8,9]]
+l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 l !! 0 !! 0
--- Main.hs
+++ Main.hs
@@ -1,1 +1,43 @@
-l = [[1,2,3],[4,5,6],[7,8,9]]
+f1:: Double->Double->Double->Double
+f1 x y z = x ** (y/z)
+-- ** là số mũ
+
+f2:: Float->Float->Float->Float
+f2 x y z = sqrt (x/y - z)
+--f2 50 2 0
+
+f3:: Bool->Bool->[Bool]
+f3 x y = [x == True] ++ [y]
+--f3 True False
+
+
+f4:: [Char]->[Char]->[Char]->Bool
+--f4:: [Int]->[Int]->[Int]->Bool
+--f4:: [Bool]->[Bool]->[Bool]->Bool
+f4 x y z = x == (y ++ z)
+--f4 "True" "Tr" "ue"
+--f4 "True" ['T','r'] ['u','e']
+
+
+
+
+{-
+thang t
+  | t `elem` [1,3,5,7,8,10,12] = "31 ngay"
+  | t == 2 = "28 ngay"
+  | t `elem` [4,6,9,11] = "30 ngay"
+  | True = "Nhap sai"
+
+
+thang1 t =
+   let
+      a = 3+5
+      b = 4+9
+    in a+b
+
+
+
+    -- let in --> tính toán nhiều công thức và chỉ cho ra 1 kết quả
+    -- where --> tính toán 1 công thức và dựa trên kết quả tính toán làm phép so sánh để cho ra nhiều kết quả bằng chữ.
+
+    -}
